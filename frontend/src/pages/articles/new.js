911f3bc75c4f71139823c7f09e1513bb112cc8d8
tikutikukotoba <tikutikukotoba12090616@gmail.com>
2024-12-16 03:06:10 +0000

    a

--- frontend/src/pages/articles/new.js
+++ frontend/src/pages/articles/new.js
@@ -1,23 +1,45 @@
+import { parse } from 'marked'; // marked.jsをインポート
+import DOMPurify from 'dompurify'; // DOMPurifyをインポート
 import mustache from 'mustache';
-// Viteのルールとして、インポートする対象のファイルをそのまま取得するためには相対パスの末尾に"?raw"を付与する必要がある
-// この場合、テンプレートのHTMLファイルをそのまま取得したいので"?raw"を末尾に付与している
-// 参照: https://ja.vite.dev/guide/assets.html#importing-asset-as-string
 import html from '../../templates/articles/new.html?raw';
-
-// 当授業ではCSRF攻撃に対して脆弱なコードとなっていますが、実装が煩雑になるので考慮せずに実装しますが
-// 実際にログインを伴うサイト等でフォーム送信などを行う処理にはCSRF攻撃に対する対策CSRFトークンも含めるなどの対策を実施してください
-// 参考: https://developer.mozilla.org/ja/docs/Glossary/CSRF
 
 /**
  * 記事新規作成時の処理の関数
  */
 export const articlesNew = () => {
   const app = document.querySelector('#app');
-  // templates/articles/new.html を <div id="app"></div> 要素内に出力する
   app.innerHTML = mustache.render(html, {});
 
-  // TODO: new.htmlにかかれているHTMLに入力の変更があったら画面右側のプレビューの内容を入力した内容に応じたものに変換する
-  // 処理...
+  const textarea = document.getElementById('editor-textarea');
+  const previewArea = document.getElementById('preview-area');
+
+  // 入力内容が変更されるたびにプレビューを更新
+  textarea.addEventListener('input', function () {
+    const markdownText = textarea.value;
+    const htmlText = DOMPurify.sanitize(parse(markdownText)); // マークダウンをHTMLに変換してサニタイズ
+    previewArea.innerHTML = htmlText; // サニタイズされたHTMLを表示
+  });
   
-  // "公開" ボタンを押下された際にPOSTメソッドで /api/v1/articles に対してAPI通信を fetch で送信する
+  document.querySelector('#articles-new-from').addEventListener('submit',(event) =>{
+    event.preventDefault();
+    console.log(event.target.titel.value);
+    console.log(event.target.body.value);
+    fetch('/api/v1/articles', {
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({
+        titel:event.target.titel.value,
+        body:event.target.body.value
+      }),
+      method: 'POST',
+    })
+    .then(res =>
+      new Promise((resolve, reject) => res.ok ? resolve(res.json()) : reject(res.text())))
+    .then(json => {
+      if(isSuccess){
+        navigate('/mypage');
+      }else{
+
+      };
+    })
+  });
 };
